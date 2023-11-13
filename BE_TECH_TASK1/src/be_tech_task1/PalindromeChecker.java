0c6ce6b9827a7b4fbd60b7d1618008bd40d9e4b4
GROSANIA <88773053+GROSANIA@users.noreply.github.com>
2023-11-13 08:35:37 +0700

    final checking

---
+++ BE_TECH_TASK1/src/be_tech_task1/PalindromeChecker.java
@@ -0,0 +1,33 @@
+package be_tech_task1;
+import java.util.Scanner;
+
+
+public class PalindromeChecker {
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to check if it's a palindrome: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPalindrome(input)) {
+            System.out.println("The entered string is a palindrome.");
+        } else {
+            System.out.println("The entered string is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        str = str.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
